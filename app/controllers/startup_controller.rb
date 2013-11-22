ee4d41d3d592beb5d019d4cd394ff56773db7d1c
jojo89 <burtoftallbiz@gmail.com>
2013-11-21 22:53:01 -0600

    set up mods and migs and route

---
+++ app/controllers/startup_controller.rb
@@ -0,0 +1,13 @@
+class StartupController < ApplicationController
+  def new
+    @startup = Startup.new
+  end	
+
+  def fetch
+  	
+  end	
+
+  def create
+    
+  end	
+end
---
+++ db/migrate/20131122043345_create_startups.rb
@@ -0,0 +1,14 @@
+class CreateStartups < ActiveRecord::Migration
+  def change
+    create_table :startups do |t|
+      t.string :name
+      t.string :logo_url
+      t.string :state
+      t.text :description
+      t.text :pitch
+      t.text :markets
+      t.string :angel_url	
+      t.timestamps
+    end
+  end
+end
