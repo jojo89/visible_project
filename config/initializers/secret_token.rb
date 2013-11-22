0d31c7e160b5fe7a41bf55cee0bc1afaecd4ccc3
jojo89 <burtoftallbiz@gmail.com>
2013-11-21 22:14:39 -0600

    first first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VisibleProject::Application.config.secret_key_base = '395680a285a6d8639bf6e02a272c31529f9680de3bc051ccc7fb10327fbd816d4078c32ad736deff80c5ec6e6cf5ef8e202740577f0bcf0f7ee72ecdccecf89b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VisibleProject::Application.config.session_store :cookie_store, key: '_visible_project_session'
